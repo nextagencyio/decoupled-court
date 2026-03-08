1cdbc835b0a02f93cb4b2a899f39f15e546ed1ae
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/judges/page.tsx
+++ app/judges/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import JudgeCard from '../components/JudgeCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Judges | Westfield District Court',
